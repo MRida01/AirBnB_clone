d59de31757e6071cec11cf984523b1a60a03c07a
Mohamed Rida Oukili <lolridalol@gmail.com>
2024-03-10 19:19:01 -0700

    updates

--- models/base_model.py
+++ models/base_model.py
@@ -12,13 +12,30 @@ class BaseModel:
         updated_at (datetime): The datetime when the instance is last updated.
     """
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """
         Initializes a new instance of the BaseModel class.
+        
+        If kwargs is not empty:
+            - Each key of this dictionary is an attribute name.
+            - Each value of this dictionary is the value of this attribute name.
+        Otherwise:
+            - Creates id and created_at as done previously.
+        
+        Args:
+            *args: Not used.
+            **kwargs: Keyword arguments representing attribute names and values.
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != '__class__':
+                    if key in ['created_at', 'updated_at']:
+                        value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """
